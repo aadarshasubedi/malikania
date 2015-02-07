be2f6c42eac8de578011049bd897d4ebd7ef47f7
David Demelier <markand@malikania.fr>
2015-02-07 16:33:39 +0100

    Initial import

---
+++ cmake/Macros.cmake
@@ -0,0 +1,275 @@
+#
+# CMakeLists.txt -- CMake build system for malikania
+#
+# Copyright (c) 2013, 2014 David Demelier <markand@malikania.fr>
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+
+include(CMakeParseArguments)
+
+# ---------------------------------------------------------
+# The following macros are available:
+#
+# malikania_generate_book
+# -----------------------
+#
+# malikania_generate_book(
+#	name			The target name (book-${name})
+#	output			The output file name
+#	sources			The sources files, kept in order
+# )
+#
+# Generate a book using pandoc.
+#
+# malikania_generate_uml
+# ----------------------
+#
+# malikania_generate_uml(
+#	NAME			The target name
+#	DIRECTORY		Output directory (relative to docs_BINARY_DIR)
+#	SOURCES			The sources files
+# )
+#
+# Generate a target uml-${NAME} with all ${SOURCES} specified to be processed by plantuml.
+#
+# malikania_define_executable
+# ---------------------------
+#
+# malikania_define_executable(
+#	PROJECT			The project name
+#	TARGET			The target name
+#	SOURCES			The list of sources
+#	FLAGS			(Optional) List of flags
+#	INCLUDES		(Optional) List of include directories
+#	LIBRARIES		(Optional) List of libraries
+# )
+#
+# Create an executable. Be sure to quote SOURCES, if not only the first file will be passed.
+# If you need flags, just pass them without -D or /D, this is automatically done for you.
+#
+# malikania_define_library
+# ------------------------
+#
+# malikania_define_library(
+#	PROJECT			The project name
+#	TARGET			The target name
+#	SOURCES			The sources
+#	FLAGS			(Optional) List of flags
+#	PRIVATE_INCLUDES	(Optional) List of includes only for building the library
+#	PUBLIC_INCLUDES		(Optional) List of public includes to share with the library users
+#	LIBRARIES		(Optional) List of libraries to link against
+# )
+#
+# Create a shared library. Follow the same specification as malikania_define_executable.
+# However, additional PRIVATE_INCLUDES and PUBLIC_INCLUDES are available.
+#
+# malikania_define_backend
+# ------------------------
+#
+# malikania_define_backend(
+#	PROJECT			The project name
+#	TARGET			The target name
+#	SOURCES			The sources
+#	FLAGS			(Optional) List of flags
+#	LIBRARIES		(Optional) List of libraries to link against
+# )
+#
+# Exactly the same as malikania_define_executable.
+#
+# setg
+# ----
+#
+# setg(var value)
+#
+# Set a cache variable internal with name var.
+#
+# ---------------------------------------------------------
+
+function(apply_flags target flags)
+	# Add MALIKANIA_BUILD to enable declspec(dllexport) on Windows
+	if (${flags})
+		target_compile_definitions(
+			${target}
+			PRIVATE ${flags}
+			PRIVATE "MALIKANIA_BUILD"
+		)
+	endif ()
+endfunction()
+
+function(apply_public_includes target var)
+	if (${var})
+		target_include_directories(${target} PUBLIC ${${var}})
+	endif ()
+endfunction()
+
+function(apply_private_includes target var)
+	if (${var})
+		target_include_directories(${target} PRIVATE ${${var}})
+	endif ()
+endfunction()
+
+function(apply_includes target var)
+	if (${var})
+		target_include_directories(${target} PRIVATE ${${var}})
+	endif ()
+endfunction()
+
+function(apply_libraries target libs)
+	if (${libs})
+		target_link_libraries(${target} ${${libs}})
+	endif ()
+endfunction()
+
+function(check_args prefix list)
+	foreach (e ${list})
+		if (NOT ${prefix}_${e})
+			message(FATAL_ERROR "Please set `${e}' parameter")
+		endif ()
+	endforeach ()
+endfunction()
+
+function(set_label target name)
+	set_target_properties(
+		${target}
+		PROPERTIES
+		PROJECT_LABEL ${name}
+	)
+endfunction()
+
+function(malikania_define_executable)
+	set(singleArgs PROJECT TARGET)
+	set(multiArgs SOURCES FLAGS INCLUDES LIBRARIES)
+	set(mandatory PROJECT TARGET SOURCES)
+
+	cmake_parse_arguments(EXE "" "${singleArgs}" "${multiArgs}" ${ARGN})
+	check_args(EXE ${mandatory})
+
+	project(${EXE_PROJECT})
+	add_executable(${EXE_TARGET} ${EXE_SOURCES})
+
+	apply_libraries(${EXE_TARGET} EXE_LIBRARIES)
+	apply_includes(${EXE_TARGET} EXE_INCLUDES)
+	apply_flags(${EXE_TARGET} EXE_FLAGS)
+endfunction()
+
+function(malikania_define_library)
+	set(singleArgs PROJECT TARGET)
+	set(multiArgs SOURCES FLAGS PRIVATE_INCLUDES PUBLIC_INCLUDES LIBRARIES)
+	set(mandatory PROJECT TARGET SOURCES)
+
+	cmake_parse_arguments(LIB "" "${singleArgs}" "${multiArgs}" ${ARGN})
+	check_args(LIB ${mandatory})
+
+	project(${LIB_PROJECT})
+	add_library(${LIB_TARGET} SHARED ${LIB_SOURCES})
+	set_label(${LIB_TARGET} ${LIB_PROJECT})
+
+	# Remove lib suffix to avoid conflict with client and libclient targets
+	set_target_properties(
+		${LIB_TARGET}
+		PROPERTIES
+		PREFIX ""
+	)
+
+	apply_libraries(${LIB_TARGET} LIB_LIBRARIES)
+	apply_private_includes(${LIB_TARGET} LIB_PRIVATE_INCLUDES)
+	apply_public_includes(${LIB_TARGET} LIB_PUBLIC_INCLUDES)
+	apply_flags(${LIB_TARGET} LIB_FLAGS)
+endfunction()
+
+function(malikania_define_backend)
+	set(singleArgs PROJECT TARGET)
+	set(multiArgs SOURCES FLAGS INCLUDES LIBRARIES)
+	set(mandatory PROJECT TARGET SOURCES)
+
+	cmake_parse_arguments(MOD "" "${singleArgs}" "${multiArgs}" ${ARGN})
+	check_args(MOD ${mandatory})
+
+	project(${MOD_PROJECT})
+	add_library(${MOD_TARGET} MODULE ${MOD_SOURCES})
+	set_label(${MOD_TARGET} ${MOD_PROJECT})
+
+	# Remove suffix as it's a module
+	set_target_properties(
+		${MOD_TARGET}
+		PROPERTIES
+		PREFIX ""
+	)
+
+	target_link_libraries(${MOD_TARGET} libclient)
+
+	apply_libraries(${MOD_TARGET} MOD_LIBRARIES)
+	apply_includes(${MOD_TARGET} MOD_INCLUDES)
+	apply_flags(${MOD_TARGET} MOD_FLAGS)
+
+	install(TARGETS ${MOD_TARGET} DESTINATION ${BACKEND_PATH})
+endfunction()
+
+function(malikania_generate_uml)
+	set(options "")
+	set(oneValueArgs NAME DIRECTORY)
+	set(multiValueArgs SOURCES)
+
+	cmake_parse_arguments(UML "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+	foreach (f ${UML_SOURCES})
+		get_filename_component(filename ${f} NAME_WE)
+		list(
+			APPEND commands
+			COMMAND ${Java_JAVA_EXECUTABLE} -jar ${PLANTUML_JAR} ${f} -o ${docs_BINARY_DIR}/uml
+		)
+
+		list(
+			APPEND outputs
+			${docs_BINARY_DIR}/uml/${filename}.png
+		)
+
+		list(APPEND sources ${f})
+	endforeach ()
+
+	add_custom_command(
+		OUTPUT ${outputs}
+		DEPENDS ${sources}
+		${commands}
+		VERBATIM
+	)
+
+	add_custom_target(
+		TARGET uml-${UML_NAME}
+		SOURCES ${sources}
+		DEPENDS ${outputs}
+	)
+
+	add_dependencies(uml uml-${UML_NAME})
+endfunction()
+
+function(malikania_generate_book name output sources)
+	pandoc(
+		book-${name}
+		SOURCES ${sources}
+		OUTPUT ${docs_BINARY_DIR}/books/${output}
+		FROM markdown
+		TO latex
+		TOC
+		STANDALONE
+		MAKE_DIRECTORY
+		WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+	)
+
+	add_dependencies(books book-${name})
+endfunction()
+
+macro(setg var value)
+	set(${var} ${value} CACHE INTERNAL "" FORCE)
+endmacro()
---
+++ cmake/internal/Options.cmake
@@ -0,0 +1,82 @@
+#
+# CMakeLists.txt -- CMake build system for malikania
+#
+# Copyright (c) 2013, 2014 David Demelier <markand@malikania.fr>
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+
+# ---------------------------------------------------------
+# Directories options
+#
+# The following options are available:
+#    WITH_BACKEND_PATH	- Path where to install backends, defaults to PREFIX + "lib/malikania/backends".
+# ---------------------------------------------------------
+
+set(WITH_BACKEND_PATH "lib/malikania/backends"
+	CACHE STRING "Where to install backends")
+
+# ---------------------------------------------------------
+# Database support
+#
+# The following options are available:
+#    WITH_POSTGRESQL	- Enable PostgreSQL backend.
+#    WITH_MYSQL		- Enable MySQL backend.
+#    WITH_SQLITE	- Enable SQLite backend.
+# ---------------------------------------------------------
+
+option(WITH_POSTGRESQL "Enable PostgreSQL" On)
+option(WITH_MYSQL "Enable MySQL" On)
+option(WITH_SQLITE "Enable SQLite" On)
+
+# ---------------------------------------------------------
+# Documentation and technical references
+#
+# The following options are available:
+#    WITH_DOCS			- Disable or enable all docs, if set to Off, disable all documentation.
+#    WITH_DOCS_UML		- Enable UML diagram generation.
+#    WITH_DOCS_DOXYGEN		- Enable doxygen
+#    WITH_DOCS_CONVENTIONS	- Enable coding conventions
+#    WITH_DOCS_SPECS		- Enable build of malikania specifications
+# ---------------------------------------------------------
+
+option(WITH_DOCS "Build all documentation" On)
+option(WITH_DOCS_UML "Enable UML diagrams generation" On)
+option(WITH_DOCS_DOXYGEN "Enable doxygen generation" On)
+option(WITH_DOCS_CONVENTIONS "Enable coding conventions" On)
+option(WITH_DOCS_SPECS "Enable specifications generation" On)
+
+if (NOT WITH_DOCS)
+	set(WITH_DOCS_UML Off)
+	set(WITH_DOCS_DOXYGEN Off)
+	set(WITH_DOCS_SPECS Off)
+endif ()
+
+# ---------------------------------------------------------
+# Targets to build
+#
+# The following options are available:
+#    WITH_LIBCOMMON	- Disable building of libcommon, if set to Off, disable all targets.
+#    WITH_LIBCLIENT	- Build the client library.
+#    WITH_LIBSERVER	- Build the server library.
+# ---------------------------------------------------------
+
+option(WITH_LIBCOMMON "Build libcommon" On)
+option(WITH_LIBCLIENT "Build libclient" On)
+option(WITH_LIBSERVER "Build libserver" On)
+
+# Ensure dependencies
+if (NOT WITH_LIBCOMMON)
+	set(WITH_LIBCLIENT Off)
+	set(WITH_LIBSERVER Off)
+endif ()
