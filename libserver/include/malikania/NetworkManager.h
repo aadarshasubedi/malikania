f95727e4180779fc56619b33149785b8ad9dd91a
David Demelier <markand@malikania.fr>
2015-03-16 06:48:14 +0100

    Import network manager

---
+++ libserver/include/malikania/NetworkManager.h
@@ -0,0 +1,37 @@
+#ifndef _MALIKANIA_NETWORK_MANAGER_H_
+#define _MALIKANIA_NETWORK_MANAGER_H_
+
+#include <atomic>
+#include <map>
+#include <memory>
+#include <thread>
+#include <vector>
+
+#include <malikania/SocketSsl.h>
+#include <malikania/SocketTcp.h>
+
+namespace malikania {
+
+class NetworkClient;
+class ServerSettings;
+
+class NetworkManager {
+private:
+	SocketTcp m_master;
+	SocketSsl m_sslMaster;
+	std::thread m_thread;
+	std::map<Socket, std::shared_ptr<NetworkClient>> m_clients;
+	std::atomic<bool> m_running{true};
+
+	void accept(Socket &s);
+	void process(Socket &s, int direction);
+	bool isMaster(const Socket &) const noexcept;
+	void run();
+
+public:
+	NetworkManager(const ServerSettings &);
+};
+
+} // !malikania
+
+#endif // !_MALIKANIA_NETWORK_MANAGER_H_
